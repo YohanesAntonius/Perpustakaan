4f0cde9883952f35d3258a11eec4be45ca599da3
Yohanes Antonius <49809783+YohanesAntonius@users.noreply.github.com>
2020-09-08 00:54:12 +0700

    Create Tambah.php

---
+++ Tambah.php
@@ -0,0 +1,10 @@
+ <div class="panel-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h3>Basic Form Examples</h3>
+                                    <form role="form">
+                                        <div class="form-group">
+                                            <label>Text Input</label>
+                                            <input class="form-control" />
+                                            <p class="help-block">Help text here.</p>
+                                        </div>
